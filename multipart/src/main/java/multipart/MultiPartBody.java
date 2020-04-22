77885b817ef3f68287ee2def7b705105aff5e197
Justin Chuk <justinhc_chuk@yahoo.com.hk>
2020-04-23 09:04:31 +1200

    made multipart json body

---
+++ multipart/src/main/java/multipart/MultiPartBody.java
@@ -0,0 +1,16 @@
+package main.java.multipart;
+import java.io.InputStream;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.core.MediaType;
+
+import org.jboss.resteasy.annotations.providers.multipart.PartType;
+public class MultiPartBody {
+    @FormParam("file")
+    @PartType(MediaType.APPLICATION_OCTET_STREAM)
+    public InputStream file;
+
+    @FormParam("fileName")
+    @PartType(MediaType.TEXT_PLAIN)
+    public String fileName;
+}
--- multipart/src/test/java/multipart/NativejsonIT.java
+++ multipart/src/test/java/multipart/NativejsonIT.java
@@ -1,9 +1,9 @@
-package org.acme.getting.started;
+package multipart;
 
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
+public class NativejsonIT extends jsonTest {
 
     // Execute the same tests but in native mode.
 }
