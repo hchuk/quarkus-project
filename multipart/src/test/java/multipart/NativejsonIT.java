deeeb9bbabfe7c0f859c3e0f4f0c59f86f3da6e6
Justin Chuk <justinhc_chuk@yahoo.com.hk>
2020-04-23 09:40:25 +1200

    clear branch

--- multipart/src/test/java/multipart/NativejsonIT.java
+++
@@ -1,9 +0,0 @@
-package multipart;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativejsonIT extends jsonTest {
-
-    // Execute the same tests but in native mode.
-}
